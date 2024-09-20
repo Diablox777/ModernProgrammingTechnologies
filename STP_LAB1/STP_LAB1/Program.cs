5896a3362496b3bd3c41c3f65572a2207be5375d
Diablox777 <tetyakov2@gmail.com>
2024-09-21 00:51:15 +0700

    Корректировка лабораторной - 5ый вариант

--- STP_LAB1/STP_LAB1/Program.cs
+++ STP_LAB1/STP_LAB1/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Text;
-using System.Runtime.CompilerServices;
 
 namespace STP_Lab1
 {
@@ -14,231 +11,98 @@ namespace STP_Lab1
                 Console.WriteLine(mes);
             }
         }
-        static int[] GenerateArray(int length)
-        {
-            int Min = -100;
-            int Max = 100;
 
-            int[] arr = new int[length];
-            Random randNum = new Random();
-            for (int i = 0; i < length; i++)
+        // Function to calculate the product of elements at even indices
+        public static float ProductEvenIndices(float[] array)
+        {
+            if (array == null || array.Length == 0)
             {
-                arr[i] = randNum.Next(Min, Max);
-            }
-            return arr;
-        }
-        static int GenerateRand(int Min, int Max)
-        {
-            //int Min = -2;
-            //int Max = 2;
-
-            int rand;
-            Random randn = new Random();
-            rand = randn.Next(Min, Max);
-            
-            return rand;
-        }
-
-        static float[] GenerateArrayfloat(int length)
-        {
-            int Min = -100;
-            int Max = 100;
-
-            float[] arr = new float[length];
-            Random randNum = new Random();
-            for (int i = 0; i < length; i++)
-            {
-                arr[i] = randNum.Next(Min, Max);
-            }
-            return arr;
-        }
-
-        public static int[] SumArray(int[] a, int[] b)
-        {
-
-            if (a.Length != b.Length)
-            {
-                Console.WriteLine(" Length a != Length b");
-                throw new invalid_argument("Разные длины массивов!");
+                throw new invalid_argument("Array is empty or null.");
             }
 
-            int[] sums = new int[a.Length];
-            for (int i = 0; i < a.Length; i++)
+            float product = 1;
+            for (int i = 0; i < array.Length; i += 2)
             {
-                sums[i] = a[i] + b[i];
+                product *= array[i];
             }
-            return sums;
+
+            return product;
         }
+
+        // Function to cyclically shift an array
         public static void LeftShifting(float[] array, int shift)
         {
-            int LengthArray = array.Length;
-            if (LengthArray == 0)
+            int lengthArray = array.Length;
+            if (lengthArray == 0)
             {
-                Console.WriteLine("Исходный массив пустой");
-                return; 
-            }  
+                Console.WriteLine("The input array is empty");
+                return;
+            }
 
-            shift = shift % LengthArray;
-            if (shift == 0) 
+            shift = shift % lengthArray;
+            if (shift == 0)
             {
-                Console.WriteLine("Cмещение 0");
-                throw new invalid_argument("Cмещение 0!");
-            } 
+                throw new invalid_argument("Shift value is 0!");
+            }
 
             float[] temp = new float[Math.Abs(shift)];
-            
+
             if (shift < 0)
             {
                 shift = Math.Abs(shift);
-                Array.Copy(array, LengthArray - shift, temp, 0, shift);
-
-                Array.Copy(array, 0, array, shift, LengthArray - shift);
-
+                Array.Copy(array, lengthArray - shift, temp, 0, shift);
+                Array.Copy(array, 0, array, shift, lengthArray - shift);
                 Array.Copy(temp, 0, array, 0, shift);
-                
             }
             else
             {
                 Array.Copy(array, temp, shift);
-
-                Array.Copy(array, shift, array, 0, LengthArray - shift);
-
-                Array.Copy(temp, 0, array, LengthArray - shift, shift);
-            }    
+                Array.Copy(array, shift, array, 0, lengthArray - shift);
+                Array.Copy(temp, 0, array, lengthArray - shift, shift);
+            }
         }
 
-        public static int FindSubsequence(int[] vec, int[] seq)
+        // Function to find the maximum even element at even indices
+        public static int MaxEvenElementEvenIndex(int[] array, out int index)
         {
-            int vecLength = vec.Length;
-            int seqLength = seq.Length;
+            index = -1;
+            int max = int.MinValue;
 
-            if (seqLength > vecLength)
-                return -1;
-
-            for (int i = 0; i <= vecLength - seqLength; i++)
+            for (int i = 0; i < array.Length; i += 2)
             {
-                bool found = true;
-                for (int j = 0; j < seqLength; j++)
+                if (array[i] % 2 == 0 && array[i] > max)
                 {
-                    if (vec[i + j] != seq[j])
-                    {
-                        found = false;
-                        break;
-                    }
+                    max = array[i];
+                    index = i;
                 }
-                if (found)
-                {
-                    return i;
-                }
-                else
-                {
-                  //  throw new invalid_argument("Cмещение 0!");
-                }
-                    
             }
 
-            return -1;
-        }
-
-        public static int[] CreatingSubstring(int[] vec)
-        {
-            int f1 = GenerateRand(0, 20);
-            int[] seq;
-            int lengthvec = GenerateRand(0, vec.Length - 1);
-            if (f1 > 10)
+            if (index == -1)
             {
-                int lengthvec1 = GenerateRand(lengthvec, vec.Length);
-
-                //Console.Write("\n{0} {1}\n", lengthvec, lengthvec1);
-
-                seq = new int[lengthvec1 - lengthvec];
-                Array.Copy(vec, lengthvec, seq, 0, lengthvec1 - lengthvec);
+                throw new invalid_argument("No even element found at even indices.");
             }
-            else
-            {
-                seq = new int[lengthvec];
-                seq = GenerateArray(lengthvec);
-            }
-            return seq;
+
+            return max;
         }
 
         static void Main(string[] args)
         {
-            //1
-            Console.WriteLine("//1\n");
-            int[] a = GenerateArray(10);
-            int[] b = GenerateArray(10);
-            int[] c = SumArray(a, b);
-            Console.WriteLine("a");
-            
-            foreach (int i in a)
-            {
-                Console.Write("{0} ", i);
-            }
-            Console.WriteLine("\n");
-            Console.WriteLine("b");
-            
-            foreach (int i in b)
-            {
-                Console.Write("{0} ", i);
-            }
-            Console.WriteLine("\n");
-            Console.WriteLine("sum");
-            
-            foreach (int i in c)
-            {
-                Console.Write("{0} ", i);
-            }
-            Console.WriteLine("\n");
+            // Testing ProductEvenIndices
+            float[] floatArray = { 1.5f, 2.3f, 4.5f, 6.7f, 8.9f };
+            Console.WriteLine("Product of even indices: " + ProductEvenIndices(floatArray));
 
+            // Testing LeftShifting
+            Console.WriteLine("\nBefore shift:");
+            foreach (var item in floatArray) Console.Write(item + " ");
+            LeftShifting(floatArray, 2);
+            Console.WriteLine("\nAfter shift:");
+            foreach (var item in floatArray) Console.Write(item + " ");
 
-
-            //2
-            Console.WriteLine("//2\n");
-            float[] d = GenerateArrayfloat(10);
-            
-            int shift = GenerateRand(-2,2);
-            
-            Console.Write("shift = {0}\n", shift);
-            Console.WriteLine("d Before\n");
-            foreach (int i in d)
-            {
-                Console.Write("{0} ", i);
-            }
-            Console.WriteLine("\n");
-
-            LeftShifting(d, shift);
-
-            Console.WriteLine("\nd After for shifting\n");
-            foreach (int i in d)
-            {
-                Console.Write("{0} ", i);
-            }
-            Console.WriteLine("\n");
-            
-            
-            //3
-            Console.WriteLine("//3\n");
-            int[] vec = GenerateArray(30);
-            Console.WriteLine("vec\n");
-            foreach (int i in vec)
-            {
-                Console.Write("{0} ", i);
-            }
-            
-            
-            int[] seq = CreatingSubstring(vec);
-            Console.WriteLine("\nseq(substring of vec)\n");
-            foreach (int i in seq)
-            {
-                Console.Write("{0} ", i);
-            }
-            
-            Console.WriteLine("\n");
-            int find = FindSubsequence(vec, seq);
-            Console.Write("found index substring: {0} ", find);
+            // Testing MaxEvenElementEvenIndex
+            int[] intArray = { 3, 2, 4, 7, 6, 5, 8 };
+            int index;
+            int maxEven = MaxEvenElementEvenIndex(intArray, out index);
+            Console.WriteLine($"\nMax even element at even index: {maxEven}, at index: {index}");
         }
-
-        
     }
 }
--- STP_LAB1/TestProject1/UnitTest1.cs
+++ STP_LAB1/TestProject1/UnitTest1.cs
@@ -4,30 +4,16 @@ namespace Test_Lab1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestProductEvenIndices()
         {
-            int[] a = { 1, 10, 5 };
-            int[] b = { 1, 10, 5 };
-            int[] expected = { 2, 20, 10 };
-
-            int[] result = STP_Lab1.Program.SumArray(a, b);
-
-            CollectionAssert.AreEqual(expected, result);
+            float[] array = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
+            float expected = 15.0f; // 1 * 3 * 5
+            float result = STP_Lab1.Program.ProductEvenIndices(array);
+            Assert.AreEqual(expected, result);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(STP_Lab1.Program.invalid_argument))]
-        public void TestMethod2()
-        {
-            int[] a = { 3, 33, 153 };
-            int[] b = { 4, -53, 73, 33 };
-            int[] expected = { 4, 1, 2 };
-
-            int[] result = STP_Lab1.Program.SumArray(a, b);
-        }
-
-        [TestMethod]
-        public void TestMethod3()
+        public void TestLeftShifting()
         {
             float[] array = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
             float[] expected = { 3.0f, 4.0f, 5.0f, 1.0f, 2.0f };
@@ -36,51 +22,15 @@ namespace Test_Lab1
         }
 
         [TestMethod]
-        public void TestMethod4()
+        public void TestMaxEvenElementEvenIndex()
         {
-            float[] array = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
-            float[] expected = { 4.0f, 5.0f, 1.0f, 2.0f, 3.0f };
-            STP_Lab1.Program.LeftShifting(array, -2);
-            CollectionAssert.AreEqual(expected, array);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(STP_Lab1.Program.invalid_argument))]
-        public void TestMethod5()
-        {
-            float[] array = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
-            float[] expected = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
-            STP_Lab1.Program.LeftShifting(array, 0);
-            CollectionAssert.AreEqual(expected, array);
-        }
-
-        [TestMethod]
-        public void TestMethod6()
-        {
-            int[] array1 = { 1, 2, 3, 4, 5 };
-            int[] array2 = { 1, 2 };
-            int expected = 0;
-            int result = STP_Lab1.Program.FindSubsequence(array1, array2);
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TestMethod7()
-        {
-            int[] array1 = { 1, 2, 3, 4, 5 };
-            int[] array2 = { 3, 4, 5 };
-            int expected = 2;
-            int result = STP_Lab1.Program.FindSubsequence(array1, array2);
-            Assert.AreEqual(expected, result);
-        }
-        [TestMethod]
-        public void TestMethod8()
-        {
-            int[] array1 = { 1, 2, 3, 4, 5 };
-            int[] array2 = { 4, 5 };
-            int expected = 3;
-            int result = STP_Lab1.Program.FindSubsequence(array1, array2);
-            Assert.AreEqual(expected, result);
+            int[] array = { 3, 2, 4, 7, 6, 5, 8 };
+            int expectedMax = 8;
+            int expectedIndex = 6;
+            int index;
+            int result = STP_Lab1.Program.MaxEvenElementEvenIndex(array, out index);
+            Assert.AreEqual(expectedMax, result);
+            Assert.AreEqual(expectedIndex, index);
         }
     }
 }
